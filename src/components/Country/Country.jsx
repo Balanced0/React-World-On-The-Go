908eb4035db3a892a9e1a63cc0fe8c2d181d4fdb
balanced0 <alvichowdhury013@gmail.com>
2026-03-20 00:50:51 +0600

    Added conditional buttons

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
+import {useState} from 'react'
 import './Country.css'
 
 const Country = ({country}) => {
     console.log(country);
-  return (
-    <div className='country'>
-        <h4>Name: {country.name.common}</h4>
-        <div><img src={country.flags.flags.png} alt={country.flags.flags.alt}></img></div>
-        <p>Population: {country.population.population}</p>
-        <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
-    </div>
-  )
+    const[visit, setVisit] = useState(false);
+    const handleVisited = () =>{
+        if(visit){
+            setVisit(false);
+        }
+        else{
+            setVisit(true);
+        }
+    }
+    return (
+        <div className={`country ${visit && "country-visited"}`}>
+            <h4>Name: {country.name.common}</h4>
+            <div><img src={country.flags.flags.png} alt={country.flags.flags.alt}></img></div>
+            <p>Population: {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>{visit ? "Visited" : "Not Visited"}</button>
+        </div>
+    )
 }
 
 export default Country
