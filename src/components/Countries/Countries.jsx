c02d779de6f486b1997a44696fa36d4bbc8ff96a
balanced0 <alvichowdhury013@gmail.com>
2026-03-19 22:46:12 +0600

    Got the data of all the available countries in the api

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { use } from 'react';
+import Country from '../Country/Country';
 
 const Countries = ({countriesPromise}) =>{
     const countriesData = use(countriesPromise);
     const countries = countriesData.countries;
     return (
-        <div><h1>All the available Countries: {countries.length}</h1></div>
+        <div>
+            <h1>All the available Countries: {countries.length}</h1>
+            {
+                countries.map(country => <Country country={country}></Country>)
+            }
+        </div>
     );
 }
 
--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const Country = () => {
+const Country = ({country}) => {
+    console.log(country);
   return (
-    <div>Country</div>
+    <div>
+        <h4>Name: {country.name.common}</h4>
+        <div><img src={country.flags.flags.png} alt={country.flags.flags.alt}></img></div>
+        <p>Population: {country.population.population}</p>
+    </div>
   )
 }
 
