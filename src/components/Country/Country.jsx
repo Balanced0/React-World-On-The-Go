c473fbc35ff3b2916c68b84707990309188666c5
balanced0 <alvichowdhury013@gmail.com>
2026-03-19 23:58:32 +0600

    Added some styling

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import './Country.css'
 
 const Country = ({country}) => {
     console.log(country);
   return (
-    <div>
+    <div className='country'>
         <h4>Name: {country.name.common}</h4>
         <div><img src={country.flags.flags.png} alt={country.flags.flags.alt}></img></div>
         <p>Population: {country.population.population}</p>
+        <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
     </div>
   )
 }
